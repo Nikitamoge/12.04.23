4ca4471457ceddbd0b9b8a37da5f817bec718b3e
Nikitamoge <74149703+Nikitamoge@users.noreply.github.com>
2023-04-12 18:32:32 +0200

    Add files via upload

---
+++ 12.04.23.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main() {
+	setlocale(0, "UKR");
+	cout << "I love C++!\n";
+	cout << "\tI love C++!\n";
+	cout << "\t\tI love C++!\n";
+	cout << "\t\t\tI love C++!\n";
+	cout << "\t\t\t\tI love C++!\n";
+	cout << "Для продолжения нажать любую кнопку . . .";
+}
+
