91389f4a8ded5a7dec5e23254148f59bc2fd48c3
Nikei <Nikei@NIKITA>
2023-04-12 18:12:49 +0200

    Add project files.

---
+++ 12.04.23/12.04.23.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main() {
+	system("color E9");
+	setlocale(0, "UKR"); // для кириллицы
+	cout << "\"добрий день\n";
+	cout << "\tor not\n";;
+}
+
+int main() {
+	system("type c:\\CPP\\1.txt");
+		// 
+
+	system("start https://mystat.itstep.org/en/main/dashboard/page/index");
+		// открытие сайтов
+}
